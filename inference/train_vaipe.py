9d09516b82056ca6c8e03d3d2f716ff306665a99
huyenbui117 <phamgialinhlx2@gmail.com>
2022-08-18 03:42:13 +0700

    train_ok

--- inference/train_vaipe.py
+++ inference/train_vaipe.py
@@ -4,7 +4,7 @@ import subprocess
 def main(config = './inference/config.json', data_config = './inference/data_config.json'):
     cfg = json.load(open(config))
     data_cfg = json.load(open(data_config))
-    train = f"python train.py --epochs {cfg['epochs']} --device {cfg['device']} --batch-size {cfg['batch_size']} --data {cfg['data']} --img-size {cfg['img_size']}  --cfg {cfg['cfg']} --name {cfg['base_name']} --hyp {cfg['hyp']} --cache-images --weights '' --notest --exist-ok"
+    train = f"python train.py --epochs {cfg['epochs']} --device {cfg['device']} --batch-size {cfg['batch_size']} --data {cfg['data']} --img-size {cfg['img_size']}  --cfg {cfg['cfg']} --name {cfg['base_name']} --hyp {cfg['hyp']} --cache-images --weights '' --exist-ok"
     subprocess.run(train, shell=True)
 if __name__ == '__main__':
     main()
