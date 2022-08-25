609fafffcb0ac16b06efaf48dfab5ff5f3350ec2
huyenbui117 <phamgialinhlx2@gmail.com>
2022-08-25 15:25:28 +0700

    tmp

--- make_class_data.py
+++ make_class_data.py
@@ -11,9 +11,9 @@ import cv2
 
 number_class = 108
 cur = 'train'
-src_img = '/home/pill/competition/yolov7/vaipe_exif/{cur}/images/'
-src_labels = '/home/pill/competition/yolov7/vaipe_exif/{cur}/labels/'
-des = '/home/pill/competition/yolov5/pill_classify/{cur}/'
+src_img = '/home/pill/competition/yolov7/vaipe_exif/{}/images/'.format(cur)
+src_labels = '/home/pill/competition/yolov7/vaipe_exif/{}/labels/'.format(cur)
+des = '/home/pill/competition/yolov5/pill_classify/{}/'.format(cur)
 
 def read_labels(path, img_w, img_h):
   boxes = []
@@ -33,19 +33,19 @@ def read_labels(path, img_w, img_h):
       boxes.append([x1, y1, x2, y2])
   return labels, boxes 
 
-for i in range(number_class):
-    os.mkdir(des + str(i))
+# for i in range(number_class):
+#     os.mkdir(des + str(i))
 
 
-# src_img_names = os.listdir(src_img)
-# for src_img_name in src_img_names:
-#   img = mpimg.imread(src_img + src_img_name)
-#   src_label_name = src_img_name.replace('jpg', 'txt')
-#   labels, boxes = read_labels(src_labels + src_label_name, img.shape[1], img.shape[0])
-#   for i, box in enumerate(boxes):
-#     img_cropped = img[box[1]:box[3], box[0]:box[2], :]
-#     name_img_cropped = src_img_name.replace('.jpg', '_' + str(i)) + '.jpg'
-#     print(name_img_cropped)
-#     path_img_cropped = des_img + name_img_cropped
-#     plt.imsave(path_img_cropped, img_cropped)
-#     cls = name_img_cropped + " " + str(labels[i]) + "\n"
+src_img_names = os.listdir(src_img)
+for src_img_name in src_img_names:
+  img = mpimg.imread(src_img + src_img_name)
+  src_label_name = src_img_name.replace('jpg', 'txt')
+  labels, boxes = read_labels(src_labels + src_label_name, img.shape[1], img.shape[0])
+  for i, box in enumerate(boxes):
+    img_cropped = img[box[1]:box[3], box[0]:box[2], :]
+    name_img_cropped = src_img_name.replace('.jpg', '_' + str(i)) + '.jpg'
+    print(name_img_cropped)
+    path_img_cropped = des + str(labels[i]) + "/" + name_img_cropped
+    plt.imsave(path_img_cropped, img_cropped)
+    cls = name_img_cropped + " " + str(labels[i]) + "\n"
