a524e49048f7f68499798fcb2ba5ceefcf3c0428
huyenbui117 <phamgialinhlx2@gmail.com>
2022-09-02 18:12:15 +0700

    add docker bashfile

---
+++ docker.sh
@@ -0,0 +1,9 @@
+docker run --rm --gpus all \
+-v $PWD/vaipe_exif:/app/vaipe_exif \
+-v /home/pill/competition/dataset:/home/pill/competition/dataset \
+-v /home/pill/competition/yolov7/vaipe_exif/public_test_new/pill/image:/home/pill/competition/yolov7/vaipe_exif/public_test_new/pill/image \
+-v $PWD/runs:/app/runs \
+-v $PWD/inference:/app/inference \
+--shm-size 8G \
+ai4vn:latest \
+inference/preprocess.py 
