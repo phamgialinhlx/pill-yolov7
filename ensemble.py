be65f34c2fb54e1478bba6c244f0545116dba797
huyenbui117 <phamgialinhlx2@gmail.com>
2022-09-02 17:22:00 +0700

    change ensemble.py

--- ensemble.py
+++ ensemble.py
@@ -21,7 +21,12 @@ def is_overlap(box, gtboxes, iou_thres, conf_thres):
     return False
 def ensemble(base, adv, iou_thres, conf_thres, data_path, save_path):
     base_pred_imgs = base['image_name'].unique()
-    image_names = os.listdir(data_path)
+    df = pd.read_csv(data_path, header=None)
+    #split dataframe by delimiter '/'
+    df[0] = df[0].str.split('/', expand=True)[-1]
+    print(df.head())
+    image_names = []
+    print(image_names)
     name_loss = []
     for name in image_names:
         if name not in base_pred_imgs:
@@ -67,7 +72,7 @@ if __name__ == '__main__':
     parser.add_argument('--adv_model', type=str, default='', help='path to result.csv file of adv model')
     parser.add_argument('--iou_thres', type=float, default=0.65, help='iou threshold')
     parser.add_argument('--conf_thres', type=float, default=0.005, help='confidence threshold')
-    parser.add_argument('--data_path', type=str, default='', help='path to pill image data. eg public_test/pill')
+    parser.add_argument('--data_path', type=str, default='./vaipe_exif/test.txt', help='path to pill image data. eg public_test/pill')
     parser.add_argument('--save_dir', type=str, default='./runs', help='path to save ensemble.csv file')
     parser.add_argument('--save_name', type=str, default='ensemble', help='name of ensemble.csv file')
     parser.add_argument('--exist_ok', action='store_true', help='overwrite existing ensemble.csv file')
